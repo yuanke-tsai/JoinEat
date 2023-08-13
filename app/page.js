bec9cbcadc502c6f3016528329c758df083ba6b7
Max Shen <m4xshen@gmail.com>
2023-08-13 12:15:32 +0800

    refactor: extract map into a component

--- app/page.js
+++ app/page.js
@@ -1,21 +1,7 @@
-'use client';
+import Map from '@/components/Map';
 
-import GoogleMapReact from 'google-map-react';
-
-export default function SimpleMap(){
+export default function Home() {
   return (
-    <div style={{ height: '50vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_API_KEY}}
-        defaultCenter={
-          {
-            lat: 25.04,
-            lng: 121.55
-          }
-        }
-        defaultZoom={18}
-      >
-      </GoogleMapReact>
-    </div>
+    <Map />
   );
 }
