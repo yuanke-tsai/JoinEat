6cef9a2510c8df2c753170939c4843051fc0fa1b
蔡沅格 <tsai@caiyuangedeMacBook-Pro.local>
2023-08-14 18:11:03 +0800

    feat: group, searchBar and searchBar for event ID

---
+++ components/Groups.jsx
@@ -0,0 +1,10 @@
+import Group from './Group'
+import styles from '../styles/groups.module.scss'
+
+export default function Groups() {
+  return (
+    <div className={styles.groups}>
+        <Group />
+    </div>
+  )
+}
