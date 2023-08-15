3bcec775569e1c323ebaea0518ba790acd7c6c93
Max Shen <m4xshen@gmail.com>
2023-08-15 10:52:53 +0800

    refactor: refine search bar

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -4,35 +4,19 @@ import styles from "../styles/searchBar.module.scss";
 
 export default function SearchBar() {
   const [isSearch, setIsSearch] = useState(false);
-  const handleClick = (e) => {
-    e.stopPropagation();
-    setIsSearch(true);
-  };
 
   return (
-    <div className={styles.searchBar}>
-      {isSearch ? (
-        <div className={styles.searchBarExtend}>
-          <div className={styles.searchIconStartPoint}>
-            <Image
-              className={styles.searchicon}
-              width={15}
-              height={15}
-              src="/searchIcon 2.png"
-            />
-            <input className={styles.input} placeholder="搜尋" />
-          </div>
-        </div>
-      ) : (
-        <div className={styles.searchBarUNExtend}>
-          <input
-            type="image"
-            alt="Search Bar"
-            src="/searchIcon 2.png"
-            onClick={handleClick}
-          />
-        </div>
-      )}
+    <div className={`${styles.searchBar} ${isSearch && styles.extend}`}>
+      <Image
+        className={styles.searchIcon}
+        width={15}
+        height={15}
+        src="/searchIcon 2.png"
+        onClick={() => {
+          setIsSearch(true);
+        }}
+      />
+      {isSearch && <input className={styles.input} placeholder="搜尋餐廳" />}
     </div>
   );
 }
