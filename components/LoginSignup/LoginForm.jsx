a5fe1d150bf0df06c88628f06f4200a17ca0a750
Max Shen <m4xshen@gmail.com>
2023-08-17 10:03:58 +0800

    feat: signup api

--- components/LoginSignup/LoginForm.jsx
+++ components/LoginSignup/LoginForm.jsx
@@ -1,15 +1,35 @@
+import { useRef } from "react";
+import axios from "axios";
 import styles from "@/styles/login.module.scss";
 
 export default function LoginForm() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   return (
-    <form className={styles.form}>
+    <form
+      className={styles.form}
+      onSubmit={async (e) => {
+        e.preventDefault();
+
+        const data = await axios.post(
+          "https://13.54.3.89/api/1.0/users/signin",
+          {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+          },
+        );
+        console.log(data);
+        // TODO: save access_token as cookie
+      }}
+    >
       <div>
         <div>電子郵件</div>
-        <input id="email" type="email" />
+        <input ref={emailRef} id="email" type="email" />
       </div>
       <div>
         <div>密碼</div>
-        <input id="password" type="password" />
+        <input ref={passwordRef} id="password" type="password" />
       </div>
       <button type="submit" className={styles.button}>
         登入
--- components/LoginSignup/SignupForm.jsx
+++ components/LoginSignup/SignupForm.jsx
@@ -1,19 +1,36 @@
+import axios from "axios";
+import { useRef } from "react";
 import styles from "@/styles/login.module.scss";
 
 export default function SignupForm() {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   return (
-    <form className={styles.form}>
+    <form
+      className={styles.form}
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        axios.post("https://13.54.3.89/api/1.0/users/signup", {
+          name: nameRef.current.value,
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+        });
+      }}
+    >
       <div>
         <div>使用者名稱</div>
-        <input id="name" type="text" />
+        <input ref={nameRef} id="name" type="text" />
       </div>
       <div>
         <div>電子郵件</div>
-        <input id="email" type="email" />
+        <input ref={emailRef} id="email" type="email" />
       </div>
       <div>
         <div>密碼</div>
-        <input id="password" type="password" />
+        <input ref={passwordRef} id="password" type="password" />
       </div>
       <div>
         <div>再次輸入密碼</div>
