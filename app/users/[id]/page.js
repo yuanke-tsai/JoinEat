0bbb4496706ab9ecf60ebde407ed4d715276f807
Max Shen <m4xshen@gmail.com>
2023-08-21 14:41:58 +0800

    feat: integrate history API

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -10,53 +10,14 @@ import Edit from "@/components/Icons/Edit";
 import LeftArrow from "@/components/Icons/LeftArrow";
 import Group from "@/components/Group";
 import useProfile from "@/hooks/useProfile";
-
-const historyData = {
-  events: [
-    {
-      event_id: "111111111",
-      host_id: "something",
-      name: "沒人可以打敗麥噹噹",
-      shop_name: "某家麥當勞",
-      latitude: 25.0388368,
-      longitude: 121.5325665,
-      people_limit: 6,
-      people_num: 6,
-      distance: 0.0,
-      appointment_time: {
-        year: 2023,
-        month: 8,
-        date: 15,
-        hour: 18,
-        minute: 30,
-      },
-    },
-    {
-      event_id: "222222222",
-      host_id: "something",
-      name: "麥噹噹 YYDS",
-      shop_name: "麥當勞-台北濟南餐廳",
-      latitude: 25.0400737,
-      longitude: 121.53261,
-      people_limit: 4,
-      people_num: 2,
-      distance: 502.3,
-      appointment_time: {
-        year: 2023,
-        month: 8,
-        date: 15,
-        hour: 18,
-        minute: 30,
-      },
-    },
-  ],
-};
+import useHistory from "@/hooks/useHistory";
 
 export default function LoginPage({ params }) {
   const [isEditing, setIsEditing] = useState(false);
   const introductionRef = useRef(null);
   const tagsRef = useRef(null);
   const profile = useProfile(params.id);
+  const events = useHistory(params.id);
   const editable = params.id === getCookie("user_id");
 
   return (
@@ -79,7 +40,11 @@ export default function LoginPage({ params }) {
           <Edit />
         </button>
       </div>
-      <Image src="/profileIcon.png" width={80} height={80} />
+      <Image
+        src={profile?.picture ?? "/profileIcon.png"}
+        width={80}
+        height={80}
+      />
       <div className={styles.name}>{profile?.name}</div>
       {isEditing ? (
         <>
@@ -140,18 +105,19 @@ export default function LoginPage({ params }) {
       </div>
       <div className={styles.subTitle}>聚餐歷史紀錄</div>
       <div className={styles.groups}>
-        {historyData.events.map((event) => (
-          <Group
-            eventTime={event.appointment_time}
-            eventDistance={event.distance}
-            setGoEvent={null}
-            isButtonDisable
-            shop_name={event.shop_name}
-            eventName={event.name}
-            people_joined={event.people_num}
-            people_limit={event.people_limit}
-          />
-        ))}
+        {events &&
+          events.map((event) => (
+            <Group
+              eventTime={event.appointment_time}
+              eventDistance={event.distance}
+              setGoEvent={null}
+              isButtonDisable
+              shop_name={event.shop_name}
+              eventName={event.name}
+              people_joined={event.people_num}
+              people_limit={event.people_limit}
+            />
+          ))}
       </div>
     </div>
   );
